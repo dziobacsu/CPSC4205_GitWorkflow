9bb974080ecc3bad2bca58d9bac0eef7a9044131
dziobacsu <dzioba_daniel@students.columbusstate.edu>
2026-01-30 20:41:23 -0500

    Improved Formatting
    Improved formatting for student information card

--- student_info.py
+++ student_info.py
@@ -3,13 +3,15 @@ major = "Information Technology"
 graduation_year = 2026
 email = "dzioba_daniel@students.columbusstate.edu"
 hometown = "Waverly Hall, GA"
+hobby = "Videogames"
 
-print("==========================================")
+print("|========================================|")
 print("       STUDENT INFORMATION CARD")
-print("==========================================")
-print(f"Name: {name}")
-print(f"Major: {major}")
-print(f"Graduation Year: {graduation_year}")
-print(f"Email: {email}")
-print(f"Hometown: {hometown}")
-print("==========================================")
+print("|========================================|")
+print(f"✏️Name: {name}")
+print(f"📜Major: {major}")
+print(f"🎓Graduation Year: {graduation_year}")
+print(f"📧Email: {email}")
+print(f"🏠Hometown: {hometown}")
+print(f"✨Hobby: {hobby}")
+print("|----------------------------------------|")
